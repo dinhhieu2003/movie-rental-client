e1146faa4d3e31d17f974253ee1bebfaa0409db3
trhanhtu <tr.ho.anh.tu@gmail.com>
2024-07-24 17:42:49 +0700

    create: login/sign up form

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  @Input() name: string = "";
+  email: string = "";
+  password: string = "";
+  onSignUp() {
+    console.log('Sign Up Data:');
+    // Handle sign-up logic here
+  }
+
+  onSignIn() {
+    console.log('Sign In Data:');
+    // Handle sign-in logic here
+  }
+
+  navigateToForgotPasswordPage() {
+    // Navigate to the forgot password page
+  }
+
+  togglePanel() {
+    const container = document.getElementById('container');
+    if(container){
+      container.classList.toggle('right-panel-active');
+    }
+  }
 
 }
