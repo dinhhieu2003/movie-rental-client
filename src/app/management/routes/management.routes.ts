ae1b4ad282f56e761a582245510897dd0164ae37
dtoan <dtoan1612@gmail.com>
2024-07-24 22:53:42 +0700

    create: create dashboard component in management and add route inside app.routes.ts

--- src/app/management/routes/management.routes.ts
+++ src/app/management/routes/management.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from "@angular/router";
+import { DashboardComponent } from "../pages/dashboard/dashboard.component";
 
-export const managementRoutes: Routes[] = [
+export const managementRoutes: Routes = [
+    { path: 'management', component: DashboardComponent }
 
 ];
