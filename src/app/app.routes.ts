588b3245f44b114889d94615b7407013a6fafb4b
dinhhieu2003 <vuongdinhhieu2003@gmail.com>
2024-07-25 22:09:50 +0700

    create: main-header, main-footer, main-layout, route to home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+    {
+        path: '',
+        loadChildren: () => import('./main/routes/main.routes').then(m => m.mainRoutes),
+    }
 ];
