91099c5e1d775295cb841cdb34f249d6b8718286
TrungPham <trungpham160503@gmail.com>
2024-07-27 20:07:50 +0700

    create movies rental list component

---
+++ src/app/main/pages/profile-management/movies-rental-list/movies-rental-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movies-rental-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './movies-rental-list.component.html',
+  styleUrl: './movies-rental-list.component.css'
+})
+export class MoviesRentalListComponent {
+
+}
