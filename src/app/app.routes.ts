2a9ebbf58887c2e871ca3b0800ecde4e4fe1c61b
Tran Trung <tranviettrung0658@gmail.com>
2024-07-28 03:07:59 +0700

    create: create category-movie component template and add route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
-import { DashboardComponent } from './management/pages/dashboard/dashboard.component';
 
 
 export const routes: Routes = [
@@ -9,7 +8,7 @@ export const routes: Routes = [
         loadChildren: () => import('./main/routes/main.routes').then(m => m.mainRoutes),
     },
     { path: "login", component: LoginComponent },
-    { path: "management", component: DashboardComponent,
+    { path: 'management',
       loadChildren: () => import('./management/routes/management.routes').then(m => m.managementRoutes)  
     },
 ];
--- src/app/management/routes/management.routes.ts
+++ src/app/management/routes/management.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from "@angular/router";
 import { DashboardComponent } from "../pages/dashboard/dashboard.component";
 import { MovieListComponent } from "../pages/movie-list/movie-list.component";
+import { CategoryMovieComponent } from "../pages/category-movie/category-movie.component";
 
 export const managementRoutes: Routes = [
-    { path: 'management', component: DashboardComponent }
+    { path: '', component: DashboardComponent,
+        children: [
+            { path: 'film-management', component: MovieListComponent},
+            { path: 'category-management', component: CategoryMovieComponent},
+        ]
+     }
 
 ];
