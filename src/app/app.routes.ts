3814ad370e3415c8d8f33c8323c84d553382f962
dtoan <dtoan1612@gmail.com>
2024-07-27 18:45:01 +0700

    update: update dashboard component in management and add route inside app.route.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
+import { DashboardComponent } from './management/pages/dashboard/dashboard.component';
 
 
 export const routes: Routes = [
@@ -8,5 +9,8 @@ export const routes: Routes = [
         loadChildren: () => import('./main/routes/main.routes').then(m => m.mainRoutes),
     },
     { path: "login", component: LoginComponent },
+    { path: "management", component: DashboardComponent,
+      loadChildren: () => import('./management/routes/management.routes').then(m => m.managementRoutes)  
+    },
 ];
 
