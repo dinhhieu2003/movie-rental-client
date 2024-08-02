92819ec0474401f804a9f7cccb2aa9cfd4ad6f0e
trhanhtu <tr.ho.anh.tu@gmail.com>
2024-08-02 21:18:02 +0700

    modified:   src/app/app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
-import { ResetPasswordComponent } from './auth/reset-password/reset-password.component';
 
 export const routes: Routes = [
     {
