c2263be34e204747d654dce2175de9676fa61d32
dinhhieu2003 <vuongdinhhieu2003@gmail.com>
2024-08-07 00:36:05 +0700

    update: badage in header

---
+++ src/app/core/services/main/cart.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BaseResponse } from '../../models/BaseResponse.model';
+import { environment } from '../../../../environments/environments';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+  private apiUrl = environment.apiUrl + "cart"
+  constructor(private httpClient: HttpClient) {  }
+  getCart(): Observable<BaseResponse> {
+    let url = this.apiUrl + "/view";
+    return this.httpClient.get<BaseResponse>(url);
+  }
+}
